d997af751bef87f6c2c1e9510204773c8a751dd1
gae-bak <gs04198@naver.com>
2020-09-18 12:13:05 +0900

    Edit readme.md 9/18 study

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+function Food({fav}) {
+ 
+return(
+<h1>I love {fav}</h1>
+)
+}
 
 function App() {
   // return <div className="App" />;
   return (
   <div>
-    <h1>Hello!!</h1> 
+    <h3>Hello!!</h3> 
+    <Food fav="김치" />
+    <Food fav="짜장면" />
+    <Food fav="짬뽕" />
   </div>
   );
 }
